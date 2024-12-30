f52cdc7e0614515a032d540e199aae707f857520
Liam Myles <liamthemonkey92@gmail.com>
2024-12-30 21:04:43 +0000

    Base App

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css"
+import { QuickTime } from "./components/mini-games/quick-time"
+
+function App() {
+  return (
+    <div>
+      <QuickTime />
+    </div>
+  )
+}
+
+export default App
