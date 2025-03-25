5f2f6c7e3a15a5d460c993c7f4e9ce6c58c7bf8e
Liam Myles <liamthemonkey92@gmail.com>
2025-03-25 20:19:14 +0000

    Add jest-dom

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,6 +1,7 @@
 import { beforeAll } from "vitest"
 
 import { configure } from "@testing-library/react"
+import "@testing-library/jest-dom/vitest"
 
 beforeAll(() => {
   configure({ reactStrictMode: true })
