bde16b29972a5303652dd8c8d6cf4088612b0dfb
Liam Myles <liamthemonkey92@gmail.com>
2025-03-29 18:02:55 +0000

    Updates

---
+++ assets/introduction-Dhjo_8JT.js
@@ -0,0 +1,11 @@
+import{j as e}from"./jsx-runtime-CLpGMVip.js";import{useMDXComponents as i}from"./index-B1mmFQwx.js";import{M as r}from"./index-_KlJaKUu.js";import"./index-BAAwwy_G.js";import"./iframe-CL47pqMG.js";import"./index-aiH20kqd.js";import"./index-Brs1icLk.js";import"./index-ogSvIofg.js";function n(o){const t={h1:"h1",h2:"h2",p:"p",...i(),...o.components};return e.jsxs(e.Fragment,{children:[e.jsx(r,{title:"Introduction"}),`
+`,e.jsx(t.h1,{id:"introduction",children:"Introduction"}),`
+`,e.jsx(t.p,{children:"As the year rolled into 2025 I made a promise to myself that I would create 4 mini games, 1 in each quarter of 2025."}),`
+`,e.jsx(t.p,{children:"Who knows how far this project will go, however I do want to eventually make a suite of components that can used to inject a bit of whimsy into the world. I aim to one day bring these games to the standard of other headless ui libraries in terms of customisation and accessibility."}),`
+`,e.jsx(t.p,{children:"This current component doesn't align with all of my goals, but it's a little game that is an MVP to deploy for my Q1 deadline, but I hope to expand it going forward as I continue to develop other mini games."}),`
+`,e.jsx(t.h1,{id:"quick-time-game",children:"Quick Time Game"}),`
+`,e.jsx(t.h2,{id:"what-is-it",children:"What is it?"}),`
+`,e.jsx(t.p,{children:"It's a reaction game, where you must enter the keys before the timer runs out. For those quick enough, you will be rewarded with a magical surprise."}),`
+`,e.jsx(t.h2,{id:"how-to-play",children:"How to play"}),`
+`,e.jsx(t.p,{children:"Using your keyboard, use the arrow keys to beat the game, moving them in the direction indicated."}),`
+`,e.jsx(t.p,{children:"To reset and try again press [Enter]."})]})}function p(o={}){const{wrapper:t}={...i(),...o.components};return t?e.jsx(t,{...o,children:e.jsx(n,{...o})}):n(o)}export{p as default};
